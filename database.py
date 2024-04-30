9159d0a09a491b0cdd136eff63877bbcbb62d1d8
LeonMuntaner <leo.muntaner@nih.gov>
2024-04-30 15:55:50 -0400

    refactored db connection

--- database.py
+++ database.py
@@ -1,9 +1,19 @@
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-# Postgres engine instance 
-SQLALCHEMY_DATABASE_URL = "postgresql://your_username:password@server/DatabaseName"
+load_dotenv()
+
+# Get database connection details from environment variables
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+# Construct the database URL
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # Get the list of schemas in the database
